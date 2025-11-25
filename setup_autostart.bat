e073fac31a3dc02b0f6ec096dfba066558b753b4
m0usm <83815139+m0usm@users.noreply.github.com>
2025-11-25 07:54:11 +0100

    Update setup_autostart.bat

--- setup_autostart.bat
+++ setup_autostart.bat
@@ -1,58 +1,58 @@
-@echo off
+echo off
 setlocal
 
 :: ----------------------------------------------------
-:: 1. DEFINITIONEN
+:: 1. DEFINITIONS
 :: ----------------------------------------------------
-:: Der feste Pfad, wo das Skript gespeichert wird (muss existieren)
+:: The permanent path where the script will be stored
 set SCRIPT_DIR=C:\Scripts
-:: Name des PowerShell-Skripts mit dem eigentlichen Befehl
+:: Name of the PowerShell script containing the core command
 set PS_SCRIPT_NAME=spicetify_install.ps1
-:: Name des Batch-Wrapper-Skripts, das im Autostart landet
+:: Name of the Batch wrapper script that lands in Startup
 set WRAPPER_SCRIPT_NAME=spicetify_start.bat
-:: Der Speicherort für den aktuellen Benutzer-Autostart-Ordner
+:: The path to the current user's Startup folder
 set STARTUP_FOLDER="%%APPDATA%%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 :: ----------------------------------------------------
-:: 2. ERSTELLEN DES SKRIPT-ORDNERS
+:: 2. CREATE SCRIPT DIRECTORY
 :: ----------------------------------------------------
-echo Erstelle Skript-Ordner: %SCRIPT_DIR%
+echo Creating script directory: %SCRIPT_DIR%
 if not exist "%SCRIPT_DIR%" mkdir "%SCRIPT_DIR%"
 
 :: ----------------------------------------------------
-:: 3. ERSTELLEN DER POWERSHELL-INSTALLATIONSDATEI
+:: 3. CREATE POWERSHELL INSTALL FILE
 :: ----------------------------------------------------
-echo Schreibe PowerShell-Befehl nach %SCRIPT_DIR%\%PS_SCRIPT_NAME%
+echo Writing PowerShell command to %SCRIPT_DIR%\%PS_SCRIPT_NAME%
 (
     echo iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 ^| iex
 ) > "%SCRIPT_DIR%\%PS_SCRIPT_NAME%"
 
 :: ----------------------------------------------------
-:: 4. ERSTELLEN DER WRAPPER-BATCH-DATEI (fuer Autostart)
+:: 4. CREATE WRAPPER BATCH FILE (for Autostart)
 :: ----------------------------------------------------
-echo Schreibe Wrapper-Batch-Skript nach %SCRIPT_DIR%\%WRAPPER_SCRIPT_NAME%
+echo Writing wrapper batch script to %SCRIPT_DIR%\%WRAPPER_SCRIPT_NAME%
 (
     echo @echo off
     echo start /min powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\%PS_SCRIPT_NAME%"
 ) > "%SCRIPT_DIR%\%WRAPPER_SCRIPT_NAME%"
 
 :: ----------------------------------------------------
-:: 5. ERSTELLEN DER VERKNUEPFUNG IM AUTOSTART-ORDNER
+:: 5. CREATE SHORTCUT IN THE STARTUP FOLDER
 :: ----------------------------------------------------
-echo Erstelle Verknuepfung im Autostart-Ordner: %STARTUP_FOLDER%
-:: Das Ziel ist das Wrapper-Skript
+echo Creating shortcut in the Startup folder: %STARTUP_FOLDER%
+:: The target is the wrapper script
 set TARGET_PATH="%SCRIPT_DIR%\%WRAPPER_SCRIPT_NAME%"
-:: Der Name der Verknuepfung
+:: The name of the shortcut
 set LINK_NAME="Spicetify Autostart.lnk"
 
-:: Nutze PowerShell, um die Verknuepfung zu erstellen, da Batch das nicht nativ kann
+:: Use PowerShell to create the shortcut, as Batch cannot do this natively
 powershell.exe -ExecutionPolicy Bypass -Command "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(%STARTUP_FOLDER%\%LINK_NAME%); $Shortcut.TargetPath = %TARGET_PATH%; $Shortcut.Save()"
 
 :: ----------------------------------------------------
-:: 6. ABSCHLUSS
+:: 6. COMPLETION
 :: ----------------------------------------------------
 echo.
-echo Die Spicetify Autostart-Routine wurde erfolgreich eingerichtet.
-echo Sie wird beim naechsten Login ausgefuehrt.
+echo The Spicetify autostart routine has been successfully set up.
+echo It will execute on the next user login.
 echo.
 pause
